942733121cd1a13f46d7d8c167744df39c0b0c40
cyberunknown <cyberunknown@gmail.com>
2020-03-06 15:11:43 +0500

    Update and rename script2.bat to compile.bat

--- compile.bat
+++ compile.bat
@@ -1,5 +1,8 @@
+@call C:\msys64\msys2_shell.cmd -bash -lc "sh 1.sh"
+pause
 cd nginx
 @call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 %*
 @set path=C:\Program Files\NASM;%path%
 nmake.exe /f objs\Makefile
-exit
+cd ..
+@call C:\msys64\msys2_shell.cmd -bash -lc "sh 2.sh"

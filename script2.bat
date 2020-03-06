872309cafee337d50b1cd49e709d82865185f417
cyberunknown <cyberunknown@gmail.com>
2020-03-06 12:26:31 +0500

    Update script2.bat

--- script2.bat
+++ script2.bat
@@ -1,4 +1,5 @@
 cd nginx
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 %*
 @set path=C:\Program Files\NASM;%path%
-@set path=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.24.28314\bin\Hostx64\x64\;%path%
 nmake.exe /f objs\Makefile
+exit

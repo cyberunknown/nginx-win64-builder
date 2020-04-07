68081bd8527d3f5406590e61d050b8ef4c86c40f
cyberunknown <cyberunknown@gmail.com>
2020-04-07 15:42:40 +0500

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -7,5 +7,5 @@ rm -r {auto,contrib,docs,misc,objs,src}
 rm {.hgtags,Makefile}
 openssl.exe passwd -apr1 1234567890 > conf/passwd
 sed -i 's/$apr1/user1:$apr1/g' conf/passwd
-rm ../*.{sh,bat,md,png}
+rm ../*.{sh,bat,md}
 rm ../{LICENSE,.wget-hsts}

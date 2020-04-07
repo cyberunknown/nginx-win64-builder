8ea328597c032e611c467c96040173635754c301
cyberunknown <cyberunknown@gmail.com>
2020-04-07 18:28:38 +0500

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,7 +1,7 @@
 @call C:\msys64\msys2_shell.cmd -bash -lc "sh 1.sh"
 pause
 cd nginx
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 %*
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64 %*
 @set path=C:\Program Files\NASM;%path%
 nmake.exe /f objs\Makefile
 cd ..

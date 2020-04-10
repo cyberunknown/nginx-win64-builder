4fcd0bbb3517c11bc3ab3dc462365a2a36511757
cyberunknown <cyberunknown@gmail.com>
2020-04-10 12:42:38 +0500

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -20,7 +20,7 @@ sed -i 's/32/64A/g' auto/lib/openssl/makefile.msvc
 sed -i 's/do_ms.bat/do_win64a.bat/g' auto/lib/openssl/makefile.msvc
 sed -i 's/do_ms/do_win64a/g' auto/lib/openssl/makefile.msvc
 sed -i 's/W4/W3/g' auto/cc/msvc
-sed -i '14a NGX_MSVC_VER=${cl_version}' auto/cc/msvc
+sed -i "14a NGX_MSVC_VER=$cl_version" auto/cc/msvc
 sed -i '14d' auto/cc/msvc
 sed -i '15d' auto/cc/msvc
 auto/configure \

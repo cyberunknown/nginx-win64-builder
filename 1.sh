3562065c84225695119e97cf629429a6948469ed
cyberunknown <cyberunknown@gmail.com>
2020-04-10 11:43:37 +0500

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,4 @@
+cl_version=`cat cl | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'`
 nginx_latest=`wget -O- https://github.com/nginx/nginx/releases | grep -Eo 'release-[0-9\.]+.tar.gz' | sort -V | tail -1 | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p'`
 fancyindex_latest=`wget -O- https://github.com/aperezdc/ngx-fancyindex/releases | grep -Eo 'v[0-9\.]+.tar.gz' | sort -V | tail -1 | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p'`
 openssl_latest=`wget -O- https://www.openssl.org/source/ | grep -Eo 'openssl-[A-Za-z0-9\.]+.tar.gz' | sort -V | tail -1 | sed -nre 's|^[^0-9]*(([0-9]+\.)*[A-Za-z0-9]+).*|\1|p'`
@@ -19,7 +20,7 @@ sed -i 's/32/64A/g' auto/lib/openssl/makefile.msvc
 sed -i 's/do_ms.bat/do_win64a.bat/g' auto/lib/openssl/makefile.msvc
 sed -i 's/do_ms/do_win64a/g' auto/lib/openssl/makefile.msvc
 sed -i 's/W4/W3/g' auto/cc/msvc
-sed -i '14a NGX_MSVC_VER=19.25.28612' auto/cc/msvc
+sed -i '14a NGX_MSVC_VER=${cl_version}' auto/cc/msvc
 sed -i '14d' auto/cc/msvc
 sed -i '15d' auto/cc/msvc
 auto/configure \

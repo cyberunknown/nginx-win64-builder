69d9ce11ada49552dcb5fe274aae7febcddb64e8
cyberunknown <cyberunknown@gmail.com>
2020-04-10 11:40:58 +0500

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,3 +1,4 @@
+strings "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.25.28610\bin\Hostx64\x64\cl.exe" > cl
 @call C:\msys64\msys2_shell.cmd -bash -lc "sh 1.sh"
 pause
 cd nginx

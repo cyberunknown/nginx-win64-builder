2d736d373cad6ef467020df2b0379b919f9a6b1b
cyberunknown <cyberunknown@gmail.com>
2020-04-10 14:22:46 +0500

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -4,7 +4,7 @@ for /d %%F in (*) do cd "%%F" & goto :break
 strings "bin\Hostx64\x64\cl.exe" > C:\msys64\home\cl
 @call C:\msys64\msys2_shell.cmd -bash -lc "sh 1.sh"
 pause
-cd /D  %~dp0
+cd /D %~dp0
 cd nginx
 @call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64 %*
 @set path=C:\Program Files\NASM;%path%
